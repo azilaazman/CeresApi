55daae9365d3d5578af983a4e23be2f68573cd35
apple <apple@apples-MacBook-Pro.local>
2017-01-13 13:05:56 +0800

    add connection model and change the connection string

---
+++ CeresAPI/Models/Connection.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CeresAPI.Models
+{
+    public static class Connection
+    {
+        public static IMongoDatabase getMLabConnection() {
+            MongoClient client = new MongoClient("mongodb://user:password@ds127428.mlab.com:27428/ceres_unit1");
+            IMongoDatabase db = client.GetDatabase("ceres_unit1");
+            return db;
+        }
+    }
+}
